cd7b517207209870b9c5786cd1ce53530f937cdb
IMGN-DRGN <thefelainme@gmail.com>
2023-05-22 03:10:48 +0100

    neatly tucked in folder 2nd commit

--- 'use strict'.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-var prompt = require('prompt-sync')({
-    history: require('prompt-sync-history')() //open history file
-  });
-  //get some user input
-  var input = prompt()
-  prompt.history.save() //s
-console.log('123');
